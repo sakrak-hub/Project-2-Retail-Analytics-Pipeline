6259341d6d1ce09f41a4666b872779c9fe9813c1
sakrak-hub <sakra_k@outlook.com>
2026-05-06 10:19:58 +0800

    Daily Upload

--- terraform/streaming-ec2.tf
+++ terraform/streaming-ec2.tf
@@ -1,3 +1,14 @@
-resource "aws_instance" {
-    
+resource "aws_instance" "streaming-ec2"{
+    ami = "ami-0a59248a6294cece2"
+    instance_type = "t2.small"
+    subnet_id = "subnet-06914b9bd660f9ccd"
+
+    tags = {
+    Name = "Kafka-Streaming"
+  }
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub") # Path to your local public key
 }
