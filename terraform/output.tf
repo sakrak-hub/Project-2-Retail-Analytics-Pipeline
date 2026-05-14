a4ea17609ce5999d74453d86c44e7d44888a6a93
sakrak-hub <sakra_k@outlook.com>
2026-05-14 09:53:08 +0800

    Daily Upload

--- terraform/output.tf
+++ terraform/output.tf
@@ -2,3 +2,8 @@ output "instance_public_ip" {
   value = aws_instance.streaming-ec2.public_ip
   description = "The public IP of the web server"
 }
+
+output "instance_id" {
+  description = "The ID of the EC2 instance"
+  value       = aws_instance.streaming-ec2.id
+}
--- terraform/streaming-ec2.tf
+++ terraform/streaming-ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "streaming-ec2" {
   ami                    = "ami-0a59248a6294cece2"
-  instance_type          = "t3.small"
+  instance_type          = "t3.medium"
   subnet_id              = aws_subnet.retailitics-network.id
   vpc_security_group_ids = [aws_security_group.retailitics-network.id]
   key_name               = aws_key_pair.generated_key.key_name
