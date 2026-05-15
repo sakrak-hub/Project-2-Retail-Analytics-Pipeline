7422fb10e2a1aea6fe4118f7b2fef47a86abdad0
sakrak-hub <sakra_k@outlook.com>
2026-05-15 10:22:08 +0800

    Daily Upload

--- terraform/output.tf
+++ terraform/output.tf
@@ -3,7 +3,7 @@ output "instance_public_ip" {
   description = "The public IP of the web server"
 }
 
-output "instance_id" {
+output "kafka_instance" {
   description = "The ID of the EC2 instance"
   value       = aws_instance.streaming-ec2.id
 }
